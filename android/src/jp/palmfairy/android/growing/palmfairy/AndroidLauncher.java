e78dc047720a6093216c997d174cff87c440ca01
youko-wakou <appu2pudding@gmail.com>
2018-01-17 01:21:11 +0900

    bakground

--- android/src/jp/palmfairy/android/growing/palmfairy/AndroidLauncher.java
+++ android/src/jp/palmfairy/android/growing/palmfairy/AndroidLauncher.java
@@ -1,16 +1,16 @@
-//package jp.palmfairy.android.growing.palmfairy;
-//
-//import android.os.Bundle;
-//
-//import com.badlogic.gdx.backends.android.AndroidApplication;
-//import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-//import jp.palmfairy.android.growing.palmfairy.MyGdxGame;
-//
-//public class AndroidLauncher extends AndroidApplication {
-//	@Override
-//	protected void onCreate (Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-//		initialize(new MyGdxGame(), config);
-//	}
-//}
+package jp.palmfairy.android.growing.palmfairy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jp.palmfairy.android.growing.palmfairy.PalamFairyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PalamFairyGame(), config);
+	}
+}
--- core/src/jp/palmfairy/android/growing/palmfairy/GameScreen.java
+++ core/src/jp/palmfairy/android/growing/palmfairy/GameScreen.java
@@ -3,32 +3,51 @@ package jp.palmfairy.android.growing.palmfairy;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
  * Created by appu2 on 2018/01/16.
  */
 
 public class GameScreen extends ScreenAdapter {
+    static final float CAMERA_WIDTH = 10;
+    static final float CAMERA_HEIGHT = 15;
     private PalamFairyGame mPalamFairyGame;
 
     Sprite mBg;
+    OrthographicCamera mCamera;
+    FitViewport mViewPort;
+
     public GameScreen(PalamFairyGame game){
         mPalamFairyGame = game;
 //        背景画像
-        Texture bgTexture = new Texture("back.png");
+        Texture bgTexture = new Texture("bagset.png");
         mBg = new Sprite(new TextureRegion(bgTexture,0,0,540,810));
         mBg.setPosition(0,0);
+
+        mCamera = new OrthographicCamera();
+        mCamera.setToOrtho(false,CAMERA_WIDTH,CAMERA_HEIGHT);
+        mViewPort = new FitViewport(CAMERA_WIDTH,CAMERA_HEIGHT,mCamera);
     }
     @Override
     public void render(float delta){
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        mCamera.update();
+        mPalamFairyGame.batch.setProjectionMatrix(mCamera.combined);
+
         mPalamFairyGame.batch.begin();
+        mBg.setPosition(mCamera.position.x - CAMERA_WIDTH,mCamera.position.y - CAMERA_HEIGHT/2);
         mBg.draw(mPalamFairyGame.batch);
         mPalamFairyGame.batch.end();
+    }
+    @Override
+    public void resize(int width,int height){
+        mViewPort.update(width,height);
     }
 }
